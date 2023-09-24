b310a461fc9bb5084c8ef150e6d332f8ea846eed
VickDaPro <vickbishnoi@gmail.com>
2023-09-24 07:50:27 +0530

    set up logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
 import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Pefile</h1>
+      <hr />
+      <p>Profile page</p>
+      <hr />
+      <button
+        onClick={handleLogout}
+        className="p-2 rounded bg-black text-orange-400"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default ProfilePage;
